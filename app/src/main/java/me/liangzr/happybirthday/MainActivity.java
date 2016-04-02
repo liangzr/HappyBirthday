1271dff83a31b606f65771add1a8dee3190ba976
liangzr <liangzr@outlook.com>
2016-04-02 21:43:10 +0800

    add cardview

--- app/src/main/java/me/liangzr/happybirthday/MainActivity.java
+++ app/src/main/java/me/liangzr/happybirthday/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        Snackbar.make(view, R.string.snackBarToast, Snackbar.LENGTH_LONG)
             .setAction("Action", null)
             .show();
       }
